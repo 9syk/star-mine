c6a9802cf78c83e35229842d29df2eed56981bf2
9syk <chlorine17cl.udn@gmail.com>
2025-06-23 10:36:59 +0900

    form.blade.phpの追加とcreate.blade.phpの変更

--- blog/resources/views/articles/create.blade.php
+++ blog/resources/views/articles/create.blade.php
@@ -1,14 +1,8 @@
 @extends('layouts.app')
 @section('content')
 <form action="{{ route('articles.store') }}" method="post">
-@csrf 
-<dl class="form-list">
-    <dt>タイトル</dt>
-    <dd><input type="text" name="title"></dd>
-    <dt>本文</dt>
-    <dd><textarea name="body" rows="5"></textarea></dd>
-</dl>
-<button type="submit">投稿する</button>
-<a href="{{ route('articles.index') }}">キャンセル</a>
+    @include('articles.form')
+    <button type="submit">投稿する</button>
+    <a href="{{ route('articles.index') }}">キャンセル</a>
 </form>
 @endsection()
