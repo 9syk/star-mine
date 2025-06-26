47f116b326476b7d749bdffe977c8e0bce12d987
SMtomapi <toma.ysh.11@gmail.com>
2025-06-26 19:42:33 +0900

    Routing clear

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/',function () {
     return view('welcome');
 });
+
+Route::get('/articles', [ArticleController::class,'index'])->name('articles.index');
