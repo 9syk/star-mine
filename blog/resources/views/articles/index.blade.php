7eecf78d950d65eecd9c2667f340d459dfd6d29c
SMtomapi <toma.ysh.11@gmail.com>
2025-06-27 14:09:06 +0900

    ルーティング

--- blog/resources/views/articles/index.blade.php
+++ blog/resources/views/articles/index.blade.php
@@ -8,3 +8,12 @@
 </article>
 @endforeach
 @endsection()
+
+@foreach ($articles as $article)
+<article class="article-item">
+    <div class="article-title">
+        <a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a>
+    </div>
+    <div class="article-info">{{ $article->created_at }}</div>
+</article>
+@endforeach
