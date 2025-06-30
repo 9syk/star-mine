3a70fb1de89b32e59a59e5dfa29a28e2cb17314c
9syk <chlorine17cl.udn@gmail.com>
2025-06-30 11:39:52 +0900

    show.blade.phpに確認ダイアログに関する記述を追加

--- blog/resources/views/articles/show.blade.php
+++ blog/resources/views/articles/show.blade.php
@@ -6,7 +6,7 @@
     <div class="article-body">{!! nl2br(e($article->body)) !!}</div>
     <div class="article-control">
         <a href="{{ route('articles.edit', $article) }}">編集</a>
-        <form action="{{ route('articles.destroy', $article) }}" method="post">
+        <form onsubmit="return confirm('本当に削除しますか？')" action="{{ route('articles.destroy', $article) }}" method="post">
             @csrf 
             @method('delete')
             <button type="submit">削除</button>
